83118261167174cc7a3aa0b416c640b121ce6156
Rain-31 <laoniao@vip.qq.com>
2020-06-11 18:30:23 +0800

    move marshal & unmarshal to i18n-gen project

--- v1/i18n/i18n.go
+++ v1/i18n/i18n.go
@@ -1,21 +1,14 @@
 package i18n
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
 	"io"
-	"io/ioutil"
-	"path/filepath"
 	"regexp"
 	"strconv"
 	"strings"
 	"sync"
 
-	"github.com/BurntSushi/toml"
 	"golang.org/x/text/language"
 	"golang.org/x/text/message"
-	"gopkg.in/yaml.v2"
 )
 
 var p printers
@@ -35,9 +28,6 @@ type printers struct {
 type PrinterSession struct {
 	printer *message.Printer
 }
-
-// Message is translation message
-type Message map[string]string
 
 func RegistPrinter(id string, lang language.Tag) *PrinterSession {
 	session := &PrinterSession{printer: message.NewPrinter(lang)}
@@ -141,61 +131,4 @@ func Plural(cases ...interface{}) []PluralRule {
 		i++
 	}
 	return rules
-}
-
-func Unmarshal(id, path string) (*Message, error) {
-	result := &Message{}
-	fileExt := strings.ToLower(filepath.Ext(path))
-	if fileExt != ".toml" && fileExt != ".json" && fileExt != ".yaml" {
-		return result, fmt.Errorf(Sprintf(id, "File type not supported"))
-	}
-
-	buf, err := ioutil.ReadFile(path)
-	if err != nil {
-		return result, nil
-	}
-
-	if strings.HasSuffix(fileExt, ".json") {
-		err := json.Unmarshal(buf, result)
-		if err != nil {
-			return result, err
-		}
-	}
-
-	if strings.HasSuffix(fileExt, ".yaml") {
-		err := yaml.Unmarshal(buf, result)
-		if err != nil {
-			return result, err
-		}
-	}
-
-	if strings.HasSuffix(fileExt, ".toml") {
-		_, err := toml.Decode(string(buf), result)
-		if err != nil {
-			return result, err
-		}
-	}
-	return result, nil
-
-}
-
-func Marshal(v interface{}, format string) ([]byte, error) {
-	switch format {
-	case "json":
-		buffer := &bytes.Buffer{}
-		encoder := json.NewEncoder(buffer)
-		encoder.SetEscapeHTML(false)
-		encoder.SetIndent("", "  ")
-		err := encoder.Encode(v)
-		return buffer.Bytes(), err
-	case "toml":
-		var buf bytes.Buffer
-		enc := toml.NewEncoder(&buf)
-		enc.Indent = ""
-		err := enc.Encode(v)
-		return buf.Bytes(), err
-	case "yaml":
-		return yaml.Marshal(v)
-	}
-	return nil, fmt.Errorf("unsupported format: %s", format)
 }
